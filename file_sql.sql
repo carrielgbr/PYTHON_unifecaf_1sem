2e2df6174bed87d880448ccb99c10e4b718742f9
carriel <carrielgbr@gmail.com>
2025-11-16 16:05:34 -0300

    FILE UPDATE AND FIXES: file_sql.sql

--- file_sql.sql
+++ file_sql.sql
@@ -9,7 +9,7 @@ CREATE TABLE tbl_admin(
     pass_admin VARCHAR (10)
 );
 
-INSERT INTO tbl_admin (nome_admin,pass_admin) VALUES ('admin','admin');
+INSERT INTO tbl_admin (nome_admin,pass_admin) VALUES ('admin01','admin01');
 SET @ID_USER_ADMIN = LAST_INSERT_ID();
 
 SELECT * FROM tbl_admin;
@@ -17,5 +17,17 @@ SELECT * FROM tbl_admin;
 CREATE TABLE tbl_users(
 	id_users INT PRIMARY KEY AUTO_INCREMENT,
     nome_user VARCHAR (100),
-    pass_user VARCHAR (100)    
+    pass_user VARCHAR (100),
+    
+    id_admin INT
 );
+
+INSERT INTO tbl_users (nome_user, pass_user, id_admin) VALUES ('allan', 'allan', @ID_USER_ADMIN);
+SET @ID_USER_ALLAN = LAST_INSERT_ID();
+
+INSERT INTO tbl_users (nome_user, pass_user, id_admin) VALUES ('carriel', 'carriel', @ID_USER_ADMIN);
+SET @ID_USER_CARRIEL = LAST_INSERT_ID();
+
+SELECT * FROM tbl_users;
+ 
+DROP DATABASE eletric_shop;
